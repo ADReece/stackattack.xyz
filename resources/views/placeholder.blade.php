c98ecac613581b4b2ffb504063478739bff19f1c
Reece Mathieson <r.mathieson@oneandall.co.uk>
2025-01-22 19:21:04 +0000

    fix: Added vite scripts to placeholder page

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Stack Attack</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div class="flex h-screen">
