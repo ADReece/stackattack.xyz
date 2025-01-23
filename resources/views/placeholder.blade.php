a829af0fb385c858906717141bbad26b7960508d
Reece Mathieson <r.mathieson@oneandall.co.uk>
2025-01-23 01:47:22 +0000

    now pls

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="flex w-screen h-screen">
+    <div class="flex items-center justify-center w-screen h-screen">
         </div class="p-5 m-auto border-solid rounded-sm border-3 border-orange">
             <img class="max-w-50 w-sm" src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381">
         </div>
