bacc3a8c004df18bb23bb0ed697fa46977252057
Reece Mathieson <r.mathieson@oneandall.co.uk>
2025-01-23 02:04:07 +0000

    Working

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png">
     @vite('resources/css/app.css')
 </head>
-<body class="p-0 m-0">
+<body class="p-0 m-0 bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-gray-950)]/5">
     <div class="flex items-center justify-center w-screen h-screen">
         <div class="p-2 border-2 border-orange-500">
             <img 
