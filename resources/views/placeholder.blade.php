dda197d2cc9451ee55070516eac05011f6f29873
Reece Mathieson <r.mathieson@oneandall.co.uk>
2025-01-23 01:43:44 +0000

    style

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -12,7 +12,7 @@
     <div class="flex w-screen h-screen">
         <div class="m-auto">
             </div class="p-5 border-solid rounded-sm border-3 border-orange">
-                <img class="max-w-full w-md" src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381">
+                <img class="max-w-50 w-sm" src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381">
             </div>
         </div>
       </div>
