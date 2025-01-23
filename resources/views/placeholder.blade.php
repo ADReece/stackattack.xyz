f134ba48caa1df5ec776594588624237b05cc8ba
Reece Mathieson <r.mathieson@oneandall.co.uk>
2025-01-23 01:39:36 +0000

    feat: added favicon fix: Fixed icon aspect ratio

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Stack Attack</title>
+    <link rel="icon" type="image/x-icon" href="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png">
     @vite('resources/css/app.css')
 </head>
 <body>
     <div class="flex w-screen h-screen">
         <div class="m-auto">
-            </div class="border-solid border-orange">
-                <img class="m-1 w-200" src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381">
+            </div class="p-5 border-solid rounded-sm border-orange">
+                <img class="max-w-ful w-50" src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381">
             </div>
         </div>
       </div>
