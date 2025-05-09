dc6158e180262f19c0e963ad75255bf5ee185f9a
Reece <reece@ascend-digital.co.uk>
2025-05-09 01:51:03 +0100

    moar moar bonks

--- resources/js/stackattack.js
+++ resources/js/stackattack.js
@@ -4,9 +4,9 @@ import hs3 from '../sounds/heartsteel3.ogg';
 
 const sfx = [
     new Audio(hs1),
-    new Audio(hs2);
-    new Audio(hs3);
-]
+    new Audio(hs2),
+    new Audio(hs3),
+];
 document.getElementById('bonk').addEventListener('click', () => {
     var sound = Math.floor(Math.random() * sfx.length);
     audio.play(sfx[sound]);
