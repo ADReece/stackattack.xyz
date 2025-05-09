05a87662314b7fdfbc3f865dba8f80a5bdc2f3e2
Reece <reece@ascend-digital.co.uk>
2025-05-09 01:00:16 +0100

    feat: bonk

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -11,12 +11,20 @@
 <body class="p-0 m-0 bg-white dark:bg-gray-800">
     <div class="flex items-center justify-center w-screen h-screen">
         <div class="p-1 border border-orange-500">
-            <img 
+            <img onclick="play()"
             src="https://wiki.leagueoflegends.com/en-us/images/Heartsteel_item.png?be381"
             alt="BONK" 
             class="object-cover w-64 m-auto" 
           />
+          <audio id="audio" src="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"></audio>
         </div>
       </div>
+      <script>
+        const audio = new Audio({{resource_path('sounds/heartsteel.mp3')}});
+
+        function play() {
+          audio.play();
+        }
+      </script>
 </body>
 </html>
